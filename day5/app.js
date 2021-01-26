c7277669c5c5923dc99a997d722985baecbe2beb
ridhoajibx <ridhoajibx@gmail.com>
2021-01-27 04:39:52 +0700

    [ridho] - change static data to dinamic data with EJS

--- day5/app.js
+++ day5/app.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
 app.listen(3000);
-app.get('/', (req, res)=> {
-    res.send("Hello world!");
-})
+
+app.get('/', (req, res) => {
+    const items = [
+        { name: 'Book', price: 10000 },
+        { name: 'Phone', price: 5000000 },
+        { name: 'Laptop', price: 3000000 },
+    ];
+    res.render('index', { items });
+});
+
+app.get('/add-item', (req, res) => {
+    res.render('add-item');
+});
+
+app.use('/', (req, res) => {
+    res.render('error');
+});
